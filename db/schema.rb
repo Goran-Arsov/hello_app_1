36fe422565a81d99cb3df10d7d2ebc6b7814ff00
Goran-Arsov <arsphy@yahoo.com>
2019-04-06 16:28:01 +0200

    generate model Post, then migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_06_121423) do
+ActiveRecord::Schema.define(version: 2019_04_06_122211) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "category_id"
+    t.integer "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
