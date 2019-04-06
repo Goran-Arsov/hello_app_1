48dedca4a16a821f07ab4bfecd0c8a05e7f58713
Goran-Arsov <arsphy@yahoo.com>
2019-04-06 13:49:33 +0200

    add all actions to the posts_controler and new show create edit update destroy view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,23 @@
 class PostsController < ApplicationController
     def index
+        @sample_text = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
     end
-    
+
+    def new
+    end
+
+    def show
+    end
+        
+    def create
+    end
+
+    def edit 
+    end
+
+    def update
+    end
+
+    def destroy
+    end
 end
